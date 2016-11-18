11fd299d569e7d4514fde3b8d71b565aeee7211d
ronald (MacBookie Pro) <ronald@orangewise.com>
2016-11-18 22:12:55 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+exports.schemaFromApi = function (api, schema) {
+  if(!('schema' in schema)) {
+    return false;
+  }
+  schema = schema['schema'];
+  if('$ref' in schema) { 
+    var splitup = schema['$ref'].split('/');
+    var schemaName = splitup[splitup.length - 1];
+    if((schemaName in api['definitions'])) {
+      return api['definitions'][schemaName];
+    } else {
+      return false;
+    } 
+  } else {
+    return schema;
+  }
+};
